366ad02ff477223d9e488f51375b501b8e1c5e9d
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-21 15:48:41 +0300

    adding past news

--- news/views.py
+++ news/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from django.http  import HttpResponse,Http404
 from django.http  import HttpResponse
 import datetime as dt
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune, Jaey cherry')
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
 
 def news_of_day(request):
     date = dt.date.today()
@@ -18,6 +30,7 @@ def news_of_day(request):
         </html>
             '''
     return HttpResponse(html)
+
     # html = f'''
     #     <html>
     #         <body>
@@ -27,15 +40,25 @@ def news_of_day(request):
     #         '''
     # return HttpResponse(html)
 
-def convert_dates(dates):
+def past_days_news(request,past_date):
+        
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+    
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
 
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
 
     
