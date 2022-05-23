574df0a4bd7dc46e16e5e27a4e17b919cfa2aa96
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-23 10:29:22 +0300

    setting postgress database

--- tribune/settings.py
+++ tribune/settings.py
@@ -22,7 +22,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'news.apps.NewsConfig',
-    'bootstrap3',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
