fd79e29151c92d496229b15e093d20650d18d50f
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-25 14:55:56 +0300

    creating article search

--- tribune/settings.py
+++ tribune/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', default=True, cast=bool)
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
