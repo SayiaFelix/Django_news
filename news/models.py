978a0b91231437269ca73193b9029436c1d28fb9
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-26 13:37:58 +0300

    creating article search

--- news/models.py
+++ news/models.py
@@ -7,15 +7,11 @@ class Editor(models.Model):
     last_name = models.CharField(max_length = 30)
     email = models.EmailField()
     phone_number = models.CharField(max_length = 10,blank =True)
-   
 
     def __str__(self):
         return self.first_name
-
-    
     def save_editor(self):
         self.save()
-        
     # def delete_editor(self):
     #     self.delete()
 
@@ -24,7 +20,6 @@ class Editor(models.Model):
 
 class tags(models.Model):
     name = models.CharField(max_length =30)
-
     def __str__(self):
         return self.name
 
@@ -33,7 +28,7 @@ class Article(models.Model):
     post = models.TextField()
     editor = models.ForeignKey(Editor,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
-    pub_date = models.DateTimeField(auto_now_add=True) 
+    pub_date = models.DateTimeField(auto_now_add=True)
     article_image = models.ImageField(upload_to = 'articles/',null=True)
 
     @classmethod
@@ -46,8 +41,9 @@ class Article(models.Model):
     def days_news(cls,date):
         news = cls.objects.filter(pub_date__date = date)
         return news
-    
+        
     @classmethod
     def search_by_title(cls,search_term):
         news = cls.objects.filter(title__icontains=search_term)
         return news
+
