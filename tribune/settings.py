26fc34c61d4c0c92eba677d78b2702b873f82c5f
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-29 11:23:12 +0300

    fixing bugs

--- tribune/settings.py
+++ tribune/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
